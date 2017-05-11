d8d61bbc9fa88e02ef1dc8a52aad7828d1189a5a
Ioannis <yannoj2@gmail.com>
2017-05-11 08:26:26 -0400

    Added weather data

--- headlines/headlines.py
+++ headlines/headlines.py
@@ -1,25 +1,59 @@
 import feedparser
-from flask import Flask
-from flask import render_template
-from flask import request
+from flask import Flask, render_template, request
+import json
+import urllib2
+import urllib
 
 app = Flask(__name__)
+
+api_key = {}
+with open('api_keys.txt', 'r') as f:
+	l = f.readline().strip().split()
+	api_key[l[0]] = l[1]
 
 RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 			 'cnn': 'http://rss.cnn.com/rss/edition.rss',
 			 'fox': 'http://feeds.foxnews.com/foxnews/latest',
 			 'iol': 'http://www.iol.co.za/cmlink/1.640'}
 
+DEFAULTS = {'publication': 'bbc', 'city': 'London,UK'}
 
 @app.route('/')
-def get_news():
-	query = request.args.get("publication")
+def home():
+	# get customized headlines, based on user input or default
+	publication = request.args.get('publication')
+	if not publication:
+		publication = DEFAULTS['publication']
+	articles = get_news(publication)
+
+	# get customized weather based on user input or default
+	city = request.args.get('city')
+	if not city:
+		city = DEFAULTS['city']
+	weather = get_weather(city)
+	return render_template("home.html", articles=articles, weather=weather)
+
+def get_news(query):
 	if not query or query.lower() not in RSS_FEEDS:
-		publication = "bbc"
+		publication = DEFAULTS['publication']
 	else:
 		publication = query.lower()
 	feed = feedparser.parse(RSS_FEEDS[publication])
-	return render_template("home.html", articles=feed['entries'])
+	return feed['entries']
+
+def get_weather(query):
+	api_url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid={}"
+	query = urllib.quote(query)
+	url = api_url.format(query, api_key['openweathermap'])
+	data = urllib2.urlopen(url).read()
+	parsed = json.loads(data)
+	weather = None
+	if parsed.get("weather"):
+		weather = {"description":parsed["weather"][0]["description"], 
+				   "temperature":parsed["main"]["temp"], 
+				   "city":parsed["name"],
+				   "country": parsed['sys']['country']}
+	return weather
 
 if '__name__' == '__main__':
 	app.run(port=5000, debug=True)
