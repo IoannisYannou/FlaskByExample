9d59457130c942729d5146706090d2f2cf15d89f
Ubuntu <ubuntu@ip-172-31-63-233.ec2.internal>
2017-05-31 02:26:54 +0000

    Corrected indentation errors

--- crimemap/db_setup.py
+++ crimemap/db_setup.py
@@ -1,23 +1,23 @@
 import pymysql
 import dbconfig
 connection = pymysql.connect(host='localhost',
-							 user=dbconfig.db_user,
-							 passwd=dbconfig.db_password)
+				user=dbconfig.db_user,
+				passwd=dbconfig.db_password)
 try:
 	with connection.cursor() as cursor:
 		sql = "CREATE DATABASE IF NOT EXISTS crimemap"
 		cursor.execute(sql)
 		sql = """CREATE IF NOT EXISTS crimemap.crimes (
-			id int NOT NULL AUTO_INCREMENT,
-			latitude FLOAT(10,6),
-			longitutde FLOAT(10,6),
-			date DATETIME,
-			category VARCHAR(50),
-			description VARCHAR(1000),
-			updated_at TIMESTAMP,
-			PRIMARY KEY (id)
-			)"""
+id int NOT NULL AUTO_INCREMENT,
+latitude FLOAT(10,6),
+longitutde FLOAT(10,6),
+date DATETIME,
+category VARCHAR(50),
+description VARCHAR(1000),
+updated_at TIMESTAMP,
+PRIMARY KEY (id)
+)"""
 		cursor.execute(sql);
-		connection.commit()
+	connection.commit()
 finally:
-		connection.close()
+	connection.close()
